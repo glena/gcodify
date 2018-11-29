00c3a99cea49d5a11a9d21b84e2ab33ead6dfde2
German Lena <german@auth0.com>
2018-11-28 22:47:26 -0300

    CLI

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Gcodify
 
 This tool parses an image and generates a GCODE file ready to be used to laser engrave with a 3d printer
+
+## Usage
+
+```
+Usage: index [options] <file>
+
+Options:
+  -V, --version               output the version number
+  -z, --z-offset [value]      Position for the Z axis in mm. Defaults to 90mm.
+  -x, --x-offset [value]      Position for the X axis in mm. Defaults to 35mm.
+  -y, --y-offset [value]      Position for the Y axis in mm. Defaults to 35mm.
+  -w, --width [value]         Resizes the image by its width in mm (height will be automatically computed if not provided).
+  -h, --height [value]        Resizes the image by its height in mm (width will be automatically computed if not provided).
+  -p, --precission [value]    Laser beam size. Defaults to 0.01mm.
+  --laser-on-code [value]     GCODE to turn laser ON. Defaults to M106.
+  --laser-off-code [value]    GCODE to turn laser OFF. Defaults to M107.
+  -s, --laser-speed [value]   Speed for the movements while engraving. Defaults to 40 mm/s.
+  -t, --travel-speed [value]  Speed for the movements while traveling. Defaults to 200 mm/s.
+  -h, --help                  output usage information
+```
--- index.js
+++ index.js
@@ -1,43 +1,31 @@
 const fs = require('fs');
+const interface = require("./lib/interface");
 const { read, prepare } = require("./lib/image_handler");
 const { calculatePixels } = require('./lib/calcs');
 const GCode = require('./lib/gcode');
 const process = require('./lib/processor');
 
-async function run() {
+async function run(params) {
   try {
-    const xOffset = 35;
-    const yOffset = 35;
-    const zOffset = 90;
+    let image = await read(params.filename);
 
-    const width_mm = 100;
-    const height_mm = null;
-    
-    const laserPrecission = 0.01; // mm
-    const laserOnCode = 'M106';
-    const laserOffCode = 'M107';
-    const laserSpeed = 20; // mmpms
-    const travelSpeed = 90; // mmpms
-
-    const filename = './logo.jpg';
-
-    let image = await read(filename);
-
-    const width = width_mm && calculatePixels(width_mm, laserPrecission);
-    const height = height_mm && calculatePixels(height_mm, laserPrecission);
+    const width = params.width && calculatePixels(params.width, params.laserPrecission);
+    const height = params.height && calculatePixels(params.height, params.laserPrecission);
 
     image = prepare(image, width, height);
 
-    const gCode = new GCode({laserOnCode, laserOffCode, laserPrecission, laserSpeed, travelSpeed, xOffset, yOffset, zOffset});
+    const gCode = new GCode(params);
     gCode.initialise();
 
     process(gCode, image)
 
-    fs.writeFileSync(`./${filename}.gcode`, gCode.build());
+    fs.writeFileSync(`./${params.filename}.gcode`, gCode.build());
 
   } catch (e) {
     console.log('error', e);
   }
 }
 
-run();
+interface.init();
+const params = interface.getParams();
+run(params);
